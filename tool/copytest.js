07aaf5cfd57ed77bc4ae8f3294c5105030411693
sttk <sttk.xslet@gmail.com>
2018-06-03 21:45:07 +0900

    Modify a tool to build tests.

--- tool/copytest.js
+++ tool/copytest.js
@@ -4,13 +4,31 @@ var listFiles = require('./lib/list-files');
 var truncFile = require('./lib/trunc-file');
 var concatFile = require('./lib/concat-file');
 var path = require('path');
+var fs = require('fs');
 
 var srcdir = path.resolve(__dirname, '../test/orig');
 var outdir = path.resolve(__dirname, '../test');
+var prjdir = path.resolve(__dirname, '..');
 
-listFiles(srcdir).forEach(revise);
 
-function revise(fp) {
+listFiles(outdir, '.test.js').forEach(function(file) {
+  fs.unlinkSync(file);
+});
+fs.mkdirSync(srcdir);
+
+
+listFiles(path.resolve(__dirname, '../..')).forEach(function(dir) {
+  if (dir === prjdir) {
+    return;
+  }
+  listFiles(path.join(dir, 'test'), '.test.js').forEach(function(testfile) {
+    var basename = path.basename(testfile);
+    fs.copyFileSync(testfile, path.resolve(srcdir, basename));
+  });
+});
+
+
+listFiles(srcdir).forEach(function replace(fp) {
   if (path.extname(fp) === '.js') {
     var outFp = path.join(outdir, path.basename(fp));
     truncFile(outFp);
@@ -21,5 +39,10 @@ function revise(fp) {
       return data;
     });
   }
-}
+});
 
+
+listFiles(srcdir, '.test.js').forEach(function(file) {
+  fs.unlinkSync(file);
+});
+fs.rmdirSync(srcdir);
